3575394e41b8270bf810befc0eab88e13db10604
Jackon0234 <125892817+Jackon0234@users.noreply.github.com>
2026-03-05 19:52:16 +0300

    Add documentation comment to content_injector.js
    Added a file header comment to describe the purpose of the script.

--- content_injector.js
+++ content_injector.js
@@ -1,4 +1,12 @@
-const s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
-s.onload = () => s.remove();
-(document.head || document.documentElement).appendChild(s);
+/**
+ * @file content_injector.js
+ * @description Injects the main script into the Snay.io game context.
+ * Securely executes the script in the page environment to access game variables.
+ */
+
+(function () {
+    const s = document.createElement('script');
+    s.src = chrome.runtime.getURL('inject.js');
+    s.onload = () => s.remove();
+    (document.head || document.documentElement).appendChild(s);
+})();
